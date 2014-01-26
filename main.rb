af59c1a6fc1b78f8e5762aff7d730cbcb129f080
tristantao <tao.tristan@gmail.com>
2014-01-26 15:59:16 -0800

    added name and points

--- main.rb
+++ main.rb
@@ -8,22 +8,23 @@ Dir["screens/*.rb"].each {|file| require_relative file }
 SCREENS_LIST = []
 
 
-train_manip_data_screen = TrainManipDataScreen.new([])
-train_viz_data_screen = TrainVizDataScreen.new([])
-train_view_data_screen = TrainViewDataScreen.new([])
+train_manip_data_screen = TrainManipDataScreen.new([], "Train Manipulate Screen", 0)
+train_viz_data_screen = TrainVizDataScreen.new([], "Train Vizualize Screen", 0)
+train_view_data_screen = TrainViewDataScreen.new([], "Train View Screen", 0)
 
-train_screen = TrainScreen.new([train_view_data_screen, train_viz_data_screen, train_manip_data_screen])
-test_screen = TestScreen.new([]) #TODO
+train_screen = TrainScreen.new([train_view_data_screen, train_viz_data_screen, train_manip_data_screen],
+	 "Train Data Screen", 0)
+test_screen = TestScreen.new([], "Test Data Screen", 0) #TODO
 
 
-data_screen = DataScreen.new([train_screen, test_screen])
+data_screen = DataScreen.new([train_screen, test_screen]m "Data Scren")
 
 
 #Do tools update here
-tools_screen = ToolsScreen.new([]) #TODO
+tools_screen = ToolsScreen.new([], "Tools Screen", 0) #TODO
 
 
-main_screen = MainScreen.new([data_screen, tools_screen])
+main_screen = MainScreen.new([data_screen, tools_screen], "Main Screen")
 
 
 
