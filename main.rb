63a7ba5185627cbb0774b509a0829b9daebf439a
Brian Liou <brian.liou91@gmail.com>
2014-01-26 16:38:01 -0800

    organized main.rb

--- main.rb
+++ main.rb
@@ -7,25 +7,25 @@ Dir["screens/*.rb"].each {|file| require_relative file }
 
 SCREENS_LIST = []
 
+#MainScreen Node
+main_screen = MainScreen.new([data_screen, models_screen, tools_screen], "Main Screen", 0)
 
-train_manip_data_screen = TrainManipDataScreen.new([], "Train Manipulate Screen", 0)
-train_viz_data_screen = TrainVizDataScreen.new([], "Train Vizualize Screen", 0)
-train_view_data_screen = TrainViewDataScreen.new([], "Train View Screen", 0)
+#From MainScreen Node
+data_screen = DataScreen.new([train_screen, test_screen], "Data Screen", 0)
 
+tools_screen = ToolsScreen.new([], "Tools Screen", 0) #TODO Update Tools screen
+
+models_screen = ModelsScreen.new([], "Models Screen", 0)
+
+#From Data Node
 train_screen = TrainScreen.new([train_view_data_screen, train_viz_data_screen, train_manip_data_screen],
 	 "Train Data Screen", 0)
 test_screen = TestScreen.new([], "Test Data Screen", 0) #TODO
 
-
-data_screen = DataScreen.new([train_screen, test_screen], "Data Screen", 0)
-
-
-#Do tools update here
-tools_screen = ToolsScreen.new([], "Tools Screen", 0) #TODO
-
-models_screen = ModelsScreen.new([], "Models Screen", 0)
-
-main_screen = MainScreen.new([data_screen, models_screen, tools_screen], "Main Screen", 0)
+#From Train Node
+train_manip_data_screen = TrainManipDataScreen.new([], "Train Manipulate Screen", 0)
+train_viz_data_screen = TrainVizDataScreen.new([], "Train Vizualize Screen", 0)
+train_view_data_screen = TrainViewDataScreen.new([], "Train View Screen", 0)
 
 
 
