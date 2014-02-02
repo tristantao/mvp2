c3bc85cc1c0188b89c8c939642355a7674edd401
tristantao <tao.tristan@gmail.com>
2014-02-02 14:56:04 -0800

    added tools size, semi complete

--- screens/GLMScreen.rb
+++ screens/GLMScreen.rb
@@ -5,6 +5,17 @@ class GLMScreen < ActionScreen
   def initialize(reachable_target, name, points)
     super(reachable_target, name, points)
     @payload_hash = Hash.new
+
+    wiki_one = OpenStruct.new(:type => "explanation",
+        :content => "Here we're going to start a lesson to learn more about Generalized Linear Models",
+        :description => "This Wiki teaches you more details about GLM")
+
+    apply_one = OpenStruct.new(:type => "explanation",
+        :content => "Here we're going to actually fit a Generalized Linear Model to our dataset.",
+        :description => "This Wiki walks you through fitting a GLM model to your dataset")
+
+    @payload_hash['wiki'] = [wiki_one]
+    @payload_hash['apply'] = [apply_one]
   end
 
   def completion_badge
--- screens/RFScreen.rb
+++ screens/RFScreen.rb
@@ -5,6 +5,17 @@ class GLMScreen < ActionScreen
   def initialize(reachable_target, name, points)
     super(reachable_target, name, points)
     @payload_hash = Hash.new
+    wiki_one = OpenStruct.new(:type => "explanation",
+        :content => "Here we're going to start a lesson to learn more about Random Forest",
+        :description => "This Wiki teaches you more details about Random Forest")
+
+    apply_one = OpenStruct.new(:type => "explanation",
+        :content => "Here we're going to actually fit a Random Forest to our dataset.",
+        :description => "This Wiki walks you through fitting a RF model to your dataset")
+
+    @payload_hash['wiki'] = [wiki_one]
+    @payload_hash['apply'] = [apply_one]
+
   end
 
   def completion_badge
