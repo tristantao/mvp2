83097d17d2d445894838d79ab8b151f2d02ee872
Brian Wayne Liou <brian.liou91@gmail.com>
2014-02-08 20:59:04 -0800

    fixed main

--- screens/mainScreen.rb
+++ screens/mainScreen.rb
@@ -7,7 +7,7 @@ class MainScreen < Screen
     @description = "This is the main screen of the tutorial!"
     @payload_hash = Hash.new
     part_one = OpenStruct.new(:type => "header",
-    	:content => "Welcome to your first data project! Here you will learn several tools for data analysis, build models,\nand do exploratory analysis! This project is taken from Kaggle, a data science competition website. The project is called\nTitanic: Machine Learning from Disaster."
+    	:content => "Welcome to your first data project! Here you will learn several tools for data analysis, build models,\nand do exploratory analysis! This project is taken from Kaggle, a data science competition website. The project is called\nTitanic: Machine Learning from Disaster.",
     	:description => "Do we need this for main?"
     	)
     part_two = OpenStruct.new(:type => "explanation",
@@ -25,7 +25,7 @@ class MainScreen < Screen
     part_six = OpenStruct.new(:type => "finish",
     	:content => "Just to let you know this tutorial involves lots of exploring and requires you to do certain parts before\nothers! We wanted to replicate the role of a data analyst as closely as possible. Have fun and Happy Exploring!"
     	)
-    @payload_hash['readme'] = [part_one, part_second, part_third, part_four, part_five, part_six]
+    @payload_hash['readme'] = [part_one, part_two, part_three, part_four, part_five, part_six]
   end
   def visit_badge
     return "mainScreenBadge"
